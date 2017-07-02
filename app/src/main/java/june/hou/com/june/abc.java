1a0a9bcf5686010a1799bc1d616b6bd9a8c8b209
houwenhao4147 <houwenhao4147@163.com>
2017-07-02 16:55:15 +0800

    新建了 abc

---
+++ app/src/main/java/june/hou/com/june/abc.java
@@ -0,0 +1,15 @@
+package june.hou.com.june;
+
+import android.util.Log;
+
+/**
+ * Created by houwe on 2017/7/2.
+ */
+
+public class abc {
+
+    private String name = "ssss";
+
+    private String address;
+
+}
