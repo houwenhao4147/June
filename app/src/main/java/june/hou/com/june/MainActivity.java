06a2591450c3a9ccb62db095fbdf66d95e6db0e6
houwenhao4147 <houwenhao4147@163.com>
2017-07-02 16:36:19 +0800

    Initial commit

---
+++ app/src/main/java/june/hou/com/june/MainActivity.java
@@ -0,0 +1,13 @@
+package june.hou.com.june;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
