1e68b2423d4db4b747c4964b038d806cfa9624d5
houwenhao4147 <houwenhao4147@163.com>
2017-07-02 16:45:04 +0800

    添加了 log2

--- app/src/main/java/june/hou/com/june/MainActivity.java
+++ app/src/main/java/june/hou/com/june/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("tag","1245");
 
-
+        Log.e("tag", "1245");
         Log.e("tag","1245");
     }
 }
